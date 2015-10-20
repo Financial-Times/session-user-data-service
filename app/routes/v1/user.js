23fb01da156998c3815fba0fff37d337255d7665
Roland Vachter <roland.vachter@iquestgroup.com>
2015-10-20 17:34:14 +0300

    Post used instead of Patch when a user is updated.

--- app/routes/v1/user.js
+++ app/routes/v1/user.js
@@ -21,6 +21,6 @@ router.post('/updateuser', userController.updateUser);
 router.get('/emptypseudonym', userController.emptyPseudonym);
 
 
-router.patch('/userUpdated/:uuid', userController.updateUserBasicInfo);
+router.post('/userUpdated/:uuid', userController.updateUserBasicInfo);
 
 module.exports = router;

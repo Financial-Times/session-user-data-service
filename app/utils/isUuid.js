1216070f071a5549c54dec1bd0c1878e91d836fc
Roland Vachter <roland.vachter@iquestgroup.com>
2015-10-26 14:50:42 +0200

    Handle integers as well.

--- app/utils/isUuid.js
+++ app/utils/isUuid.js
@@ -1,6 +1,9 @@
 "use strict";
 
 var isUuid = function (id) {
+	if (typeof id !== 'string') {
+		id = String(id);
+	}
 	var uuidMatch = id.match(new RegExp("^([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})$", "i"));
     if (uuidMatch && uuidMatch[1]) {
         return uuidMatch[1];

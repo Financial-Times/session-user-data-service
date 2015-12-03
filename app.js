5289a609a8f0f40a7906fce4ca7ed4df1fa4096f
Roland Vachter <roland.vachter@iquestgroup.com>
2015-12-03 10:00:15 +0200

    Error logged only if status code is 5xx.

--- app.js
+++ app.js
@@ -11,25 +11,27 @@ var env = require('./env');
 var urlParser = require('url');
 
 var routes = {
-	index: require('./app/routes/index'),
-	__health: require('./app/routes/__health'),
-	__about: require('./app/routes/__about'),
-	__gtg: require('./app/routes/__gtg'),
-	v1: {
-		livefyre: require('./app/routes/v1/livefyre'),
-		user: require('./app/routes/v1/user')
-	}
+    index: require('./app/routes/index'),
+    __health: require('./app/routes/__health'),
+    __about: require('./app/routes/__about'),
+    __gtg: require('./app/routes/__gtg'),
+    v1: {
+        livefyre: require('./app/routes/v1/livefyre'),
+        user: require('./app/routes/v1/user')
+    }
 };
 
 
 if (env.logger.level) {
-	consoleLogger.setLevel(env.logger.level);
+    consoleLogger.setLevel(env.logger.level);
 }
 
 if (env.logger.filter) {
-	var filters = env.logger.filter.split(',').map(function (item) {return item.trim();});
+    var filters = env.logger.filter.split(',').map(function(item) {
+        return item.trim();
+    });
 
-	consoleLogger.addFilter(filters);
+    consoleLogger.addFilter(filters);
 }
 
 consoleLogger.enable();
@@ -38,18 +40,18 @@ consoleLogger.enable();
 var app = express();
 
 var corsOptions = {
-	origin: function(origin, callback) {
-		if (origin) {
-			var allowed = 'ft.com';
-			var hostname = urlParser.parse(origin).hostname;
+    origin: function(origin, callback) {
+        if (origin) {
+            var allowed = 'ft.com';
+            var hostname = urlParser.parse(origin).hostname;
 
-			callback(null, hostname.indexOf(allowed, hostname.length - allowed.length) !== -1);
+            callback(null, hostname.indexOf(allowed, hostname.length - allowed.length) !== -1);
 
-			return;
-		}
+            return;
+        }
 
-		callback(null, false);
-	}
+        callback(null, false);
+    }
 };
 
 app.use(cors(corsOptions));
@@ -60,45 +62,47 @@ app.set('view engine', 'hjs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
-app.use(function (req, res, next) {
-	// normalize stupid tomcat cookies, which appends ",$Version=0" after the values of each cookie
+app.use(function(req, res, next) {
+    // normalize stupid tomcat cookies, which appends ",$Version=0" after the values of each cookie
 
-	if (req.cookies) {
-		for (let cookieKey in req.cookies) {
-			if (req.cookies.hasOwnProperty(cookieKey)) {
-				if (req.cookies[cookieKey].indexOf('$Version') !== -1) {
-					req.cookies[cookieKey] = req.cookies[cookieKey].replace(/\,\$Version\=[0-9]/g, '');
-				}
-			}
-		}
-	}
-	next();
+    if (req.cookies) {
+        for (let cookieKey in req.cookies) {
+            if (req.cookies.hasOwnProperty(cookieKey)) {
+                if (req.cookies[cookieKey].indexOf('$Version') !== -1) {
+                    req.cookies[cookieKey] = req.cookies[cookieKey].replace(/\,\$Version\=[0-9]/g, '');
+                }
+            }
+        }
+    }
+    next();
 });
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (env.maintenanceModeOn) {
-	app.all('*', function (req, res, next) {
-		res.status(503).send("Maintenance");
-	});
+    app.all('*', function(req, res, next) {
+        res.status(503).send("Maintenance");
+    });
 } else {
-	app.use('/v1/livefyre', routes.v1.livefyre);
-	app.use('/v1/user', routes.v1.user);
-	app.use('/', routes.__gtg);
-	app.use('/', routes.__health);
-	app.use('/', routes.__about);
-	app.get('/', function (req, res) {
-		res.redirect('/apidoc');
-	});
+    app.use('/v1/livefyre', routes.v1.livefyre);
+    app.use('/v1/user', routes.v1.user);
+    app.use('/', routes.__gtg);
+    app.use('/', routes.__health);
+    app.use('/', routes.__about);
+    app.get('/', function(req, res) {
+        res.redirect('/apidoc');
+    });
 }
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -106,17 +110,22 @@ app.use(function(req, res, next) {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-  consoleLogger.error("ERROR", err, err.stack);
+    var status = err.status || err.statusCode || 503;
+
+    res.status(status);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+
+	if (status >= 500) {
+    	consoleLogger.error("ERROR", err, err.stack);
+    }
 });
 
 
 process.on('uncaughtException', function(err) {
-	consoleLogger.error('Uncaught EXCEPTION: ', err, err.stack);
+    consoleLogger.error('Uncaught EXCEPTION: ', err, err.stack);
 });
 
 

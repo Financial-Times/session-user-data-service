792e049964c88c436e569ef479fdbaba92f677f9
Roland Vachter <roland.vachter@iquestgroup.com>
2015-12-14 13:59:49 +0200

    Increase the timeout to services from 10 to 25 seconds.

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var cors = require('cors');
 var consoleLogger = require('./app/utils/consoleLogger');
 var env = require('./env');
 var urlParser = require('url');
+var needle = require('needle');
+
+
+needle.defaults({
+	open_timeout: 25000
+});
 
 var routes = {
     index: require('./app/routes/index'),

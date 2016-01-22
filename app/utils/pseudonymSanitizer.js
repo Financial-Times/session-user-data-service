69399a2a3daf318d147dc6919ffb262a6e804d5b
Roland Vachter <roland.vachter@iquestgroup.com>
2016-01-22 15:54:51 +0200

    Debug why allowed characters are not correctly loaded.

--- app/utils/pseudonymSanitizer.js
+++ app/utils/pseudonymSanitizer.js
@@ -4,6 +4,8 @@ const env = require('../../env');
 
 exports.getNotAllowedCharacters = function (pseudonym) {
 	let notAllowedCharacters = [];
+
+	console.log('allowedCharacters', env.validation.pseudonym.allowedCharacters);
 	for (let i=0; i<pseudonym.length; i++) {
 		if (env.validation.pseudonym.allowedCharacters.indexOf(pseudonym[i]) === -1) {
 			notAllowedCharacters.push(pseudonym[i]);

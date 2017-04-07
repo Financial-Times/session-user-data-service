249318a478c47b76894d53d070bc472684920dfc
Roland Vachter <roland.vachter@iquestgroup.com>
2017-04-07 14:52:38 +0300

    Remove healthchecks for CAPI v1 and add healthcheck for Next Elastic Search.

--- app/health/healthServices/elasticsearch.js
+++ app/health/healthServices/elasticsearch.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const _ = require('lodash');
-const capi_v1 = require('../../services/capi_v1');
+const nEsClient = require('@financial-times/n-es-client');
 const env = require('../../../env');
 const consoleLogger = require('../../utils/consoleLogger');
 
 const healthCheckModel = {
-	id: 'capi-v1',
-	name: 'Content API v1',
+	id: 'next-elastic-search',
+	name: 'Next Elastic Search',
 	ok: false,
 	technicalSummary: "Reads tags of articles (sections, authors, brand)",
 	severity: 3,
@@ -32,29 +32,23 @@ exports.getHealth = function (callback) {
 
 	var currentHealth = _.clone(healthCheckModel);
 
-	try {
-		capi_v1.getArticleData('109842b8-71f4-11e5-9b9e-690fdae72044', function (err, data) {
-			if (err) {
-				currentHealth.ok = false;
-				currentHealth.checkOutput = 'statusCode: ' + err.statusCode;
-				callCallback(null, currentHealth);
-				return;
-			}
-
+	nEsClient.get('109842b8-71f4-11e5-9b9e-690fdae72044')
+		.then(article => {
 			currentHealth.ok = true;
 			callCallback(null, _.omit(currentHealth, ['checkOutput']));
-		});
-
-		// timeout after 15 seconds
-		setTimeout(function () {
+		})
+		.catch(err => {
 			currentHealth.ok = false;
-			currentHealth.checkOutput = 'timeout';
+			currentHealth.checkOutput = 'statusCode: ' + err.statusCode;
 			callCallback(null, currentHealth);
 			return;
-		}, 10000);
-	} catch (e) {
-		consoleLogger.error('health', 'capi_v1', 'Exception', e);
+		});
+
+	// timeout after 15 seconds
+	setTimeout(function () {
 		currentHealth.ok = false;
-		currentHealth.checkOutput = 'Exception';
-	}
+		currentHealth.checkOutput = 'timeout';
+		callCallback(null, currentHealth);
+		return;
+	}, 10000);
 };
